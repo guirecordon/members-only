778d19d49e7c2dd62a3db8464ad53e85ae5c6a87
jmagero <jmagero2017@gmail.com>
2020-08-11 21:24:21 +0300

    Add nav_bar with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
 
   root 'posts#index'
 end
