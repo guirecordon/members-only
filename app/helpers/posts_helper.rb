465dee119cb497b1fbbe254da49cfa65e7578e6d
Gui Recordon <guilherme.recordon@gmail.com>
2020-08-14 15:46:11 -0300

    Add helper methods to post views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,19 @@ module PostsHelper
   def post_params
     params.require(:post).permit(:text)
   end
+
+  def user_name(post)
+    post.user.name if user_signed_in?  
+  end
+
+  def render_form
+    render 'form', post: @post if user_signed_in?
+  end
+
+  def error_message(post)
+    if post.errors.any?
+      pluralize(post.errors.count, " error") + " prohibited this post from being saved:"
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :post, presence: true
 end
