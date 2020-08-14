ec2c17f0bd3799a356f6b9823f3b20d5546719f4
Gui Recordon <guilherme.recordon@gmail.com>
2020-08-14 19:09:08 -0300

    Add a partial for flash notice method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def sign_in(user)
-       if user_signed_in?
+       if user_signed_in? 
         render 'signedin'
        else 
         render 'signedout'
        end
   end
+
+  def flash_helper
+    render 'flash' if flash[:alert]
+  end
+
 end
